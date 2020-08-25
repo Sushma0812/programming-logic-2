1603158bc778ef052f867eb7ed6bd2559ac8990b
Sushma Bhupathi <bhupathisushma1998@gmail.com>
2020-08-25 10:14:17 +0000

    Initial commit

---
+++ Zero/src/com/stackroute/Zero.java
@@ -0,0 +1,32 @@
+package com.stackroute;
+import java.util.Scanner;
+
+public class Zero{
+
+    public static void main(String[] args)
+    {
+        Scanner reader = new Scanner(System.in);
+        int number;
+        System.out.println("Enter a number");
+        number = reader.nextInt();
+        int maximum = -1;
+        int count = 0;
+        while(number>0)
+        {
+            if((number & 1) == 0)
+            {
+                count++;
+                number >>= 1;
+                maximum = Math.max(maximum,count);
+            }
+            else
+            {
+                maximum = Math.max(maximum,count);
+                count = 0;
+                number >>= 1;
+            }
+        }
+        System.out.println("Number of zeroes "+ maximum);
+
+    }
+}
